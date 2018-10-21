5ff74c9146a63eae6dd45952b8d1a57f38f1cb3e
sywang6 <947904592@qq.com>
2018-10-21 15:13:05 +0800

    初始化仓库

---
+++ src/main/java/com/pccc/mdbpay/GpckApplication.java
@@ -0,0 +1,12 @@
+package com.pccc.mdbpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpckApplication.class, args);
+	}
+}
