e0aeb406e43cc4749a5a542914cb0133e27fe33b
sywang6 <947904592@qq.com>
2018-10-21 15:29:02 +0800

    添加注释

--- src/main/java/com/pccc/mdbpay/HelloWorldController.java
+++ src/main/java/com/pccc/mdbpay/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@RequestMapping("/hello")
 	public String index() {
+		//添加注释
 		return "Hello World";
 	}
 }
